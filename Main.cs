fe444c155d5a1a64ecf0b45bcc7c81d1a3cffea1
flynhigh09 <sethlenarsich@zoomtown.com>
2015-05-19 07:31:05 -0400

    Updated more testing needed Ai 2 doesnt enter vehicle?

--- Main.cs
+++ Main.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.IO;
 using System.Windows.Forms;
 using GTA;
-using GTA.Native;
 using GTA.Math;
+using GTA.Native;
 using SPraceMod.Races;
 
 namespace SPraceMod
@@ -11,8 +15,29 @@ namespace SPraceMod
     {
         Race01 Race01 = new Race01();
 
-        string VehicleModel = "BTYPE";
+       float healthmax = Game.Player.Character.MaxHealth;
+       VehicleHash VehicleModel = VehicleHash.Dukes2;
 
+        enum Controls
+        {
+            DPAD_DOWN = 187,
+            DPAD_UP = 188,
+            DPAD_RIGHT = 190,
+            DPAD_LEFT = 189,
+            START = 199,
+            SELETE = 217,
+            X = 201,
+            Square = 203,
+            Circle = 202,
+            Tringle = 204,
+            L1 = 205,
+            R1 = 206,
+            L2 = 207,
+            R2 = 208,
+            L3 = 209,
+            R3 = 210,
+            R3Down = 198,
+        };
 
         public Main()
         {
@@ -20,32 +45,79 @@ namespace SPraceMod
             Interval = 200;
             Tick += Main_Tick;
             KeyDown += KeyDownEvent;
+            KeyUp += KeyUpEvent;
 
-            
-
-
+         //  Race01._bigmessage = new UIText("Races", new Point(630, 100), 2.8f, Color.Firebrick, 2, true);
         }
 
         private void LoadConfig()
         {
-            Settings.SetValue("RACE SETTINGS", "VEHICLE MODEL", "BODHI2");
+            Settings.SetValue("RACE SETTINGS", "VEHICLE MODEL", "ADDER");
         }
 
         void Main_Tick(object sender, EventArgs e)
         {
-            GTA.UI.ShowSubtitle("X:" + String.Format("{0:0,0.000}", Game.Player.Character.Position.X) + " Y:" + Game.Player.Character.Position.Y + " Z:" + Game.Player.Character.Position.Z + "  Rotation =" + String.Format("{0:0,0.000}", Game.Player.Character.Heading));
+           /* Race01._Start = World.CreateBlip(Race01.SpawnPointPlayer);
+            Function.Call(Hash.SET_BLIP_COLOUR, Race01._Start.Handle, 2);
+            Function.Call(Hash.SET_BLIP_ROUTE, Race01._Start.Handle, true);*/
         }
-
-
-
 
         void KeyDownEvent(object sender, EventArgs e)
         {
             if (Game.IsKeyPressed(Keys.NumPad1))
             {
-                Race01.Initialize(1, VehicleModel);
-                
+                Race01.Initialize(VehicleModel);          
+            }
+            if (Game.IsKeyPressed(Keys.K))
+            {
+                Ped player = Game.Player.Character;
+                float pos = player.Heading;
+                UI.ShowSubtitle("X:" + String.Format("{0:0,0.000}", player.Position.X) + " Y:" + player.Position.Y + " Z:" + player.Position.Z + "  Rotation =" + String.Format("{0:0,0.000}", pos));
             }
         }
+        void KeyUpEvent(object sender, EventArgs e)
+        {
+            if (Game.IsKeyPressed(Keys.NumPad4))
+            {
+                Ped player = Game.Player.Character;
+                float pos = player.Heading;
+                UI.ShowSubtitle("X:" + String.Format("{0:0,0.000}", player.Position.X) + " Y:" + player.Position.Y + " Z:" + player.Position.Z + "  Rotation =" + String.Format("{0:0,0.000}", pos));
+            }
+        }
+
+        #region Commands
+        void ActivateInvincibility()
+        {
+        }
+
+        void DeactivateInvincibility()
+        {
+        }
+
+        void HealPlayer()
+        {
+        }
+
+        void ActivateNeverWanted()
+        {
+        }
+
+        void DeactivateNeverWanted()
+        {
+        }
+
+        void ActivateUnlimitedAmmo()
+        {
+        }
+
+        void DeactivateUnlimitedAmmo()
+        {
+        }
+
+        void DoUnlockAllWeapons()
+        {
+        }
+
+        #endregion
     }
 }
--- Races/Race01.cs
+++ Races/Race01.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
 using GTA;
 using GTA.Native;
 using GTA.Math;
@@ -7,89 +12,135 @@ namespace SPraceMod.Races
 {
     class Race01 : Script
     {
+        private UIText _headsup;
+        private UIRectangle _headsupRectangle;
+        public UIText _bigmessage;
 
-        
+        public Blip _Start;
+        Blip _chks;
 
+        float racercarhealthmax = 0;
+        float racerhealthmax = 0;
+        float racerhealth = 0;
+        float racerSpeed = 100f;
 
         Vehicle RaceCar01;
+        Vehicle RaceCar02;
+        Vehicle playerRaceCar;
+        Ped playerRacer;
         Ped Racer01;
-        Vector3 SpawnPointPlayer = new Vector3(648.682f, 3502.916f, 33.27346f);
-        Vector3 Racer01Spawn = new Vector3(637.395f, 3505.451f, 35.62923f);
-        Vector3 RaceCar01Spawn = new Vector3(638.435f, 3502.02f, 33.58217f);
+        Ped Racer02;
+        
         TaskSequence Racer01Task;
+        TaskSequence Racer02Task;
 
+        private readonly List<Ped> _racers = new List<Ped>();
 
-        float racerSpeed = 200f;
+        public Vector3 SpawnPointPlayer = new Vector3(648.682f, 3502.916f, 33.27346f);
+        Vector3 Racer01Spawn = new Vector3(638.395f, 3503.451f, 35.62923f);
 
+        Vector3 RaceCar01Spawn = new Vector3(638.435f, 3518.02f, 33.58217f);
+        Vector3 RaceCar02Spawn = new Vector3(638.435f, 3522.02f, 33.58217f);
+        Vector3 RaceCar03Spawn = new Vector3(638.435f, 3526.02f, 33.58217f);
 
         Vector3 Point01 = new Vector3(412.771f, 3502.336f, 34.02979f);
         Vector3 Point02 = new Vector3(330.752f, 3583.387f, 32.6484f);
         Vector3 Point03 = new Vector3(905.267f, 3633.396f, 32.01043f);
+        private readonly Vector3[] _checkpoints = {
+        new Vector3(412.771f, 3502.336f, 34.02979f),
+        new Vector3(330.752f, 3583.387f, 32.6484f),
+        new Vector3(905.267f, 3633.396f, 32.01043f)};
 
-       
         int CurrentCheckpoint;
-
+        
         public Race01()
         {
             Interval = 250;
-            Tick += Race01_Tick;
-
-
-           
+            Tick += Race01_Tick;       
         }
-
-       
-
-        internal void Initialize(int NumberOfRacers, string VehicleModel)
+   
+        internal void Initialize(VehicleHash VehicleModel)
         {
+            playerRacer = Game.Player.Character;
 
-            RaceCar01 = World.CreateVehicle(VehicleModel, RaceCar01Spawn, 95f);
-            Racer01 = World.CreatePed(GTA.Native.PedHash.Barry, Racer01Spawn);
-            SetupTasksequences();
-            RaceCar01.MarkAsNoLongerNeeded();
-            Racer01.MarkAsNoLongerNeeded();
+            //Vehicles
+            RaceCar01 = World.CreateVehicle(VehicleModel, RaceCar01Spawn, 98.702f);
+            RaceCar02 = World.CreateVehicle(VehicleModel, RaceCar02Spawn, 98.702f);
+            playerRaceCar = World.CreateVehicle(VehicleModel, RaceCar03Spawn, 98.702f);
+
+            //PEDS
+            Racer01 = Function.Call<Ped>(Hash.CREATE_RANDOM_PED, 637.415f, 3517.02f, 35.62923f);
+            Racer02 = Function.Call<Ped>(Hash.CREATE_RANDOM_PED, 638.415f, 3519.02f, 35.62923f);
+
+            //Mods
+            RaceCar01.NeonLightsColor = Color.FromArgb(55, 255, 130);
+            playerRaceCar.NeonLightsColor = Color.FromArgb(75, 99, 175);
+            RaceCar02.NeonLightsColor = Color.FromArgb(50, 190, 30);
+
+            RaceCar01.SetNeonLightsOn(VehicleNeonLight.Left,true);
+            RaceCar01.SetNeonLightsOn(VehicleNeonLight.Right, true);
+
+            RaceCar02.SetNeonLightsOn(VehicleNeonLight.Right, true);
+            RaceCar02.SetNeonLightsOn(VehicleNeonLight.Left, true);
+
+            playerRaceCar.SetNeonLightsOn(VehicleNeonLight.Left, true);
+            playerRaceCar.SetNeonLightsOn(VehicleNeonLight.Right, true);
+            playerRaceCar.NumberPlate = "Fly";
+            playerRaceCar.CustomPrimaryColor = Color.FromArgb(25, 130, 210);
+            playerRaceCar.CustomSecondaryColor = Color.FromArgb(199, 22, 20);
+
+            //Tasks
+            playerRacer.Task.WarpIntoVehicle(playerRaceCar, VehicleSeat.Driver);
+
+            RacerTask();
+            Racer2Task();
+
+            Racer02.Task.PerformSequence(Racer02Task);
             Racer01.Task.PerformSequence(Racer01Task);
-            OnCheckpoint(0);
-            
+
+            //Race?
+            OnCheckpoint(3);            
         }
-        void SetupTasksequences()
+
+        void Racer2Task()
+        {
+            Racer02Task = new TaskSequence();
+            
+            Racer02Task.AddTask.WarpIntoVehicle(RaceCar02, VehicleSeat.Driver);
+            Racer02Task.AddTask.DriveTo(RaceCar02, Point01, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+            Racer02Task.AddTask.DriveTo(RaceCar02, Point02, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+           // Racer02Task.AddTask.FollowPointRoute(Point03);
+        }
+        void RacerTask()
         {
             Racer01Task = new TaskSequence();
-            Racer01Task.AddTask.WarpIntoVehicle(RaceCar01, VehicleSeat.Driver);
-            Racer01Task.AddTask.DriveTo(RaceCar01, Point01, 100f, (int)DrivingStyle.AvoidTrafficExtremely);
-            Racer01Task.AddTask.DriveTo(RaceCar01, Point02, 300f, (int)DrivingStyle.AvoidTrafficExtremely);
-          //  Racer01Task.AddTask.DriveTo(RaceCar01, Point03, 300f, (int)DrivingStyle.AvoidTrafficExtremely);
-            
-            Racer01Task.Close();
+
+            Racer01Task.AddTask.WarpIntoVehicle(RaceCar01, VehicleSeat.Driver);                
+            Racer01Task.AddTask.DriveTo(RaceCar01, Point01, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+            Racer01Task.AddTask.DriveTo(RaceCar01, Point02, racerSpeed, (int)DrivingStyle.AvoidTraffic);            
+           // Racer01Task.AddTask.FollowPointRoute(Point03);
         }
-
-
 
         void Race01_Tick(object sender, EventArgs e)
         {
-            if (Game.Player.Character.Position.DistanceTo(Point01) < 10f && CurrentCheckpoint == 0)
+            if (playerRacer.Position.DistanceTo(Point01) < 10f && CurrentCheckpoint == 0)
             {
-
                 CurrentCheckpoint += 1;
                 OnCheckpoint(1);
 
                 UI.Notify("Reached Checkpoint 1/NAN");
             }
-            if (Game.Player.Character.Position.DistanceTo(Point02) < 10f && CurrentCheckpoint == 1)
+            if (playerRacer.Position.DistanceTo(Point02) < 10f && CurrentCheckpoint == 1)
             {
-
                 CurrentCheckpoint += 1;
                 OnCheckpoint(2);
 
-
                 UI.Notify("Reached Checkpoint 2/NAN");
             }
-            if (Game.Player.Character.Position.DistanceTo(Point03) < 10f && CurrentCheckpoint == 1)
+            if (playerRacer.Position.DistanceTo(Point03) < 10f && CurrentCheckpoint == 1)
             {
-
                 CurrentCheckpoint += 1;
                 OnCheckpoint(3);
-
 
                 UI.Notify("Reached Checkpoint 3/NAN");
             }
@@ -97,26 +148,25 @@ namespace SPraceMod.Races
 
         void OnCheckpoint(int cpointID)
         {
+            for(int i = 0; i < Getcheckpoints().Length(); i++)
+            //_chks = World.CreateBlip(i);
+            Function.Call(Hash.ADD_BLIP_FOR_COORD, i);
+            Function.Call(Hash.SET_BLIP_COLOUR, _chks.Handle, 1);
+            Function.Call(Hash.SET_BLIP_ROUTE, _chks.Handle, true);
 
-           // Blip Checkpoint = World.CreateBlip(new Vector3(0f, 0f, 0f), 8f);
+             racercarhealthmax = RaceCar01.MaxHealth;
+             racerhealthmax = Racer01.MaxHealth;
+             racerhealth = Racer01.Health;
 
-            if(cpointID == 0)
+            if (cpointID == 0)
             {
-                //Checkpoint.Position = Point01;
-                if (Game.Player.Character.IsInVehicle())
-                {
-                    Vehicle oldCar = Game.Player.Character.CurrentVehicle;
-                    oldCar.Delete();
-                }
-
-                Vehicle NewCar = World.CreateVehicle("BTYPE", SpawnPointPlayer, 98.702f);
-                Game.Player.Character.Task.WarpIntoVehicle(NewCar, VehicleSeat.Driver);
-
-                
+                if (racercarhealthmax <= 0)
+                    racerhealth = 0;
+               // Checkpoint.Position = Point01;     
             }
             if (cpointID == 1 && CurrentCheckpoint == 1)
             {
-                //Checkpoint.Position = Point02;
+               // Checkpoint.Position = Point02;
             }
             if (cpointID == 2 && CurrentCheckpoint == 2)
             {
@@ -124,5 +174,48 @@ namespace SPraceMod.Races
             }
         }
 
+        private Vector3 Getcheckpoints()
+        {
+            var lastDist = float.MaxValue;
+            Vector3 outputDist = new Vector3(0.0f, 0.0f, 0.0f);
+            foreach (var chcks in _checkpoints)
+            {
+                if ((chcks - Racer01.Position).Length() < lastDist)
+                {
+                    outputDist = chcks;
+                    lastDist = (chcks - Racer01.Position).Length();
+                }
+            }      
+            return outputDist;
+        }
+
+        Ped randomracers(int NumberOfRacers)
+        {
+               int[] peds = {NumberOfRacers};
+               Racer01 = Function.Call<Ped>(Hash.CREATE_RANDOM_PED, 637.415f, 3517.02f, 35.62923f);
+            return Racer01;
+        }
+
+        Ped GetRandompeds()
+        {
+            OutputArgument outArg = new OutputArgument();
+            Racer01 = Function.Call<Ped>(Hash.CREATE_RANDOM_PED, outArg, 637.415f, 3517.02f, 35.62923f);
+            Ped output = outArg.GetResult<Ped>();
+            return output;
+        }
+
+        void AddCash(int amount)
+        {
+            string statNameFull = string.Format("SP{0}_TOTAL_CASH", 
+            (Game.Player.Character.Model.Hash == new Model("player_zero").Hash) ? 0 :    //Michael
+            (Game.Player.Character.Model.Hash == new Model("player_one").Hash) ? 1 :     //Franklin
+            (Game.Player.Character.Model.Hash == new Model("player_two").Hash) ? 2 : 0); //Trevor
+
+            int name = Function.Call<int>(Hash.GET_HASH_KEY, statNameFull);
+            OutputArgument outArg = new OutputArgument();
+            Function.Call<bool>(Hash.STAT_GET_INT, name, outArg, -1);
+            var cash = outArg.GetResult<int>() + amount;
+            Function.Call(Hash.STAT_SET_INT, name, cash, true);
+        }
     }
 }
