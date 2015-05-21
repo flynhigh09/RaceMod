60fa160a7b7ab1934c4678ea784ea943c768ce1c
flynhigh09 <sethlenarsich@zoomtown.com>
2015-05-20 20:17:55 -0400

    Updated / Working
    Still needs testing added some mods to vehicles.

--- Main.cs
+++ Main.cs
@@ -8,15 +8,18 @@ using GTA;
 using GTA.Math;
 using GTA.Native;
 using SPraceMod.Races;
+using System.ComponentModel;
 
 namespace SPraceMod
 {
     public class Main : Script 
     {
         Race01 Race01 = new Race01();
+        Ped playerRacer;
 
-       float healthmax = Game.Player.Character.MaxHealth;
-       VehicleHash VehicleModel = VehicleHash.Dukes2;
+        VehicleHash VehicleModel;
+        String Vheicle;
+        ScriptSettings _settings;
 
         enum Controls
         {
@@ -39,6 +42,42 @@ namespace SPraceMod
             R3Down = 198,
         };
 
+        Boolean isPressed(int Button)
+        {
+            InputArgument button = new InputArgument(Button);
+            return Function.Call<Boolean>(Hash.IS_CONTROL_JUST_PRESSED, 2, button);
+        }
+        Boolean isJustPress(int Button)
+        {
+            InputArgument button = new InputArgument(Button);
+            return Function.Call<Boolean>(Hash.IS_CONTROL_PRESSED, 2, button);
+        }
+
+        Boolean isReleased(int Button)
+        {
+            InputArgument button = new InputArgument(Button);
+            return Function.Call<Boolean>(Hash.IS_CONTROL_JUST_RELEASED, 2, button);
+        }
+        Boolean isJustReleased(int Button)
+        {
+            InputArgument button = new InputArgument(Button);
+            return Function.Call<Boolean>(Hash.IS_DISABLED_CONTROL_JUST_RELEASED, 0, button);
+        }
+
+        bool isDown(int Button)
+        {
+            OutputArgument buttons = new OutputArgument();
+            InputArgument button = new InputArgument(Button);
+          return Function.Call<bool>(Hash.IS_DISABLED_CONTROL_PRESSED, 0, button);
+           // bool output = buttons.GetResult<bool>();
+          //  return output;
+        }
+        Boolean isJustDown(int Button)
+        {
+            InputArgument button = new InputArgument(Button);
+            return Function.Call<Boolean>(Hash.IS_DISABLED_CONTROL_JUST_PRESSED, 0, button);
+        }
+
         public Main()
         {
             LoadConfig();
@@ -52,7 +91,8 @@ namespace SPraceMod
 
         private void LoadConfig()
         {
-            Settings.SetValue("RACE SETTINGS", "VEHICLE MODEL", "ADDER");
+              ScriptSettings _settings = ScriptSettings.Load("SPraceMod.net");
+              VehicleModel = _settings.GetValue<VehicleHash>("[RACE_SETTINGS]", "VEHICLE", VehicleHash.Dukes2);
         }
 
         void Main_Tick(object sender, EventArgs e)
@@ -66,18 +106,19 @@ namespace SPraceMod
         {
             if (Game.IsKeyPressed(Keys.NumPad1))
             {
-                Race01.Initialize(VehicleModel);          
+                Race01.Initialize(VehicleModel);
             }
-            if (Game.IsKeyPressed(Keys.K))
+            if (Game.IsKeyPressed(Keys.NumPad3))
             {
-                Ped player = Game.Player.Character;
-                float pos = player.Heading;
-                UI.ShowSubtitle("X:" + String.Format("{0:0,0.000}", player.Position.X) + " Y:" + player.Position.Y + " Z:" + player.Position.Z + "  Rotation =" + String.Format("{0:0,0.000}", pos));
+                playerRacer = Game.Player.Character;
+                var pos = playerRacer.Position;
+                var pos2 = pos.Around(10f);
+                Function.Call(Hash.SHOOT_SINGLE_BULLET_BETWEEN_COORDS, pos.X, pos.Y, pos.Z, pos2.X, pos2.Y, pos2.Z, 15, true, (int)playerRacer.Weapons[WeaponHash.AssaultRifle].Hash, playerRacer.Handle, 1, 1, 1f);
             }
         }
         void KeyUpEvent(object sender, EventArgs e)
         {
-            if (Game.IsKeyPressed(Keys.NumPad4))
+            if (Game.IsKeyPressed(Keys.K))
             {
                 Ped player = Game.Player.Character;
                 float pos = player.Heading;
--- Races/Race01.cs
+++ Races/Race01.cs
@@ -12,17 +12,11 @@ namespace SPraceMod.Races
 {
     class Race01 : Script
     {
-        private UIText _headsup;
         private UIRectangle _headsupRectangle;
-        public UIText _bigmessage;
-
+        public static UIText _bigmessage;
+            
         public Blip _Start;
         Blip _chks;
-
-        float racercarhealthmax = 0;
-        float racerhealthmax = 0;
-        float racerhealth = 0;
-        float racerSpeed = 100f;
 
         Vehicle RaceCar01;
         Vehicle RaceCar02;
@@ -36,12 +30,17 @@ namespace SPraceMod.Races
 
         private readonly List<Ped> _racers = new List<Ped>();
 
+        float racercarhealthmax = 0;
+        float racerhealthmax = 0;
+        float racerhealth = 0;
+        float racerSpeed = 100f;
+
         public Vector3 SpawnPointPlayer = new Vector3(648.682f, 3502.916f, 33.27346f);
         Vector3 Racer01Spawn = new Vector3(638.395f, 3503.451f, 35.62923f);
 
         Vector3 RaceCar01Spawn = new Vector3(638.435f, 3518.02f, 33.58217f);
         Vector3 RaceCar02Spawn = new Vector3(638.435f, 3522.02f, 33.58217f);
-        Vector3 RaceCar03Spawn = new Vector3(638.435f, 3526.02f, 33.58217f);
+        Vector3 RaceCar03Spawn = new Vector3(638.435f, 3515.02f, 33.58217f);
 
         Vector3 Point01 = new Vector3(412.771f, 3502.336f, 34.02979f);
         Vector3 Point02 = new Vector3(330.752f, 3583.387f, 32.6484f);
@@ -52,15 +51,28 @@ namespace SPraceMod.Races
         new Vector3(905.267f, 3633.396f, 32.01043f)};
 
         int CurrentCheckpoint;
-        
+
+        Random rnd;
+
         public Race01()
         {
             Interval = 250;
-            Tick += Race01_Tick;       
+            Tick += Race01_Tick;
+            rnd = new Random();
         }
    
+        Color Getrandomcolor()
+        {
+            return Color.FromArgb(rnd.Next(0,255), rnd.Next(0, 255), rnd.Next(0, 255));
+        }
+        int getrandomnumb(int nlow, int nhigh)
+        {
+            return rnd.Next(nlow, nhigh);
+        }
+
         internal void Initialize(VehicleHash VehicleModel)
         {
+            UI.ShowSubtitle("~b~Race to Checkpoint 1 ~s~");
             playerRacer = Game.Player.Character;
 
             //Vehicles
@@ -73,62 +85,86 @@ namespace SPraceMod.Races
             Racer02 = Function.Call<Ped>(Hash.CREATE_RANDOM_PED, 638.415f, 3519.02f, 35.62923f);
 
             //Mods
-            RaceCar01.NeonLightsColor = Color.FromArgb(55, 255, 130);
-            playerRaceCar.NeonLightsColor = Color.FromArgb(75, 99, 175);
-            RaceCar02.NeonLightsColor = Color.FromArgb(50, 190, 30);
+            RaceCar01.NeonLightsColor = Getrandomcolor();
+            playerRaceCar.NeonLightsColor = Getrandomcolor();
+            RaceCar02.NeonLightsColor = Getrandomcolor();
 
             RaceCar01.SetNeonLightsOn(VehicleNeonLight.Left,true);
             RaceCar01.SetNeonLightsOn(VehicleNeonLight.Right, true);
 
             RaceCar02.SetNeonLightsOn(VehicleNeonLight.Right, true);
             RaceCar02.SetNeonLightsOn(VehicleNeonLight.Left, true);
-
+          
             playerRaceCar.SetNeonLightsOn(VehicleNeonLight.Left, true);
             playerRaceCar.SetNeonLightsOn(VehicleNeonLight.Right, true);
-            playerRaceCar.NumberPlate = "Fly";
-            playerRaceCar.CustomPrimaryColor = Color.FromArgb(25, 130, 210);
-            playerRaceCar.CustomSecondaryColor = Color.FromArgb(199, 22, 20);
+            playerRaceCar.NumberPlate = "FlynHi";
+            Function.Call(Hash.SET_VEHICLE_NUMBER_PLATE_TEXT_INDEX, playerRaceCar, 5);
+
+            playerRaceCar.CustomPrimaryColor = Getrandomcolor();
+            playerRaceCar.CustomSecondaryColor = Getrandomcolor();
+            RaceCar02.CustomPrimaryColor = Getrandomcolor();
+            RaceCar02.CustomSecondaryColor = Getrandomcolor();
+            RaceCar01.CustomPrimaryColor = Getrandomcolor();
+            RaceCar01.CustomSecondaryColor = Getrandomcolor();
+
+            playerRaceCar.SetMod(VehicleMod.Engine, (int)VehicleModel, true);
+            playerRaceCar.SetMod(VehicleMod.Transmission, (int)VehicleModel, true);
+                
+            Function.Call(Hash._0x93A3996368C94158, playerRaceCar, 10000.0f);//SET_VEHICLE_ENGINE_POWER_MULTIPLIER
+            //Function.Call(Hash.SET_VEHICLE_LIVERY, playerRaceCar, 5);
+            Function.Call(Hash.SET_VEHICLE_MOD_COLOR_1, playerRaceCar, 5, 0, 0);
+            Function.Call(Hash.SET_VEHICLE_WHEEL_TYPE, playerRaceCar, getrandomnumb(1,7));
 
             //Tasks
             playerRacer.Task.WarpIntoVehicle(playerRaceCar, VehicleSeat.Driver);
 
             RacerTask();
-            Racer2Task();
 
-            Racer02.Task.PerformSequence(Racer02Task);
+            RaceCar01.MarkAsNoLongerNeeded();
+            RaceCar02.MarkAsNoLongerNeeded();
+            Racer01.MarkAsNoLongerNeeded();
+            Racer02.MarkAsNoLongerNeeded();
+
             Racer01.Task.PerformSequence(Racer01Task);
-
+            Racer02.Task.PerformSequence(Racer02Task);
+                    
             //Race?
-            OnCheckpoint(3);            
+            OnCheckpoint(0);            
         }
 
-        void Racer2Task()
-        {
-            Racer02Task = new TaskSequence();
-            
-            Racer02Task.AddTask.WarpIntoVehicle(RaceCar02, VehicleSeat.Driver);
-            Racer02Task.AddTask.DriveTo(RaceCar02, Point01, racerSpeed, (int)DrivingStyle.AvoidTraffic);
-            Racer02Task.AddTask.DriveTo(RaceCar02, Point02, racerSpeed, (int)DrivingStyle.AvoidTraffic);
-           // Racer02Task.AddTask.FollowPointRoute(Point03);
-        }
         void RacerTask()
         {
             Racer01Task = new TaskSequence();
 
-            Racer01Task.AddTask.WarpIntoVehicle(RaceCar01, VehicleSeat.Driver);                
-            Racer01Task.AddTask.DriveTo(RaceCar01, Point01, racerSpeed, (int)DrivingStyle.AvoidTraffic);
-            Racer01Task.AddTask.DriveTo(RaceCar01, Point02, racerSpeed, (int)DrivingStyle.AvoidTraffic);            
-           // Racer01Task.AddTask.FollowPointRoute(Point03);
+            Racer01Task.AddTask.WarpIntoVehicle(RaceCar01, VehicleSeat.Driver);
+            Racer01Task.AddTask.DriveTo(RaceCar01, Point01, 5f, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+            Racer01Task.AddTask.DriveTo(RaceCar01, Point02, 5f, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+            Racer01Task.AddTask.DriveTo(RaceCar01, Point03, 5f, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+
+            Racer01Task.Close();
+
+            Racer02Task = new TaskSequence();
+
+            Racer02Task.AddTask.WarpIntoVehicle(RaceCar02, VehicleSeat.Driver);
+            Racer02Task.AddTask.DriveTo(RaceCar02, Point01, 5f, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+            Racer02Task.AddTask.DriveTo(RaceCar02, Point02, 5f, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+            Racer02Task.AddTask.DriveTo(RaceCar02, Point03, 5f, racerSpeed, (int)DrivingStyle.AvoidTraffic);
+
+            Racer02Task.Close();
         }
 
         void Race01_Tick(object sender, EventArgs e)
         {
+            racercarhealthmax = RaceCar01.MaxHealth;
+            racerhealthmax = Racer01.MaxHealth;
+            racerhealth = Racer01.Health;
+        
             if (playerRacer.Position.DistanceTo(Point01) < 10f && CurrentCheckpoint == 0)
-            {
+            {              
                 CurrentCheckpoint += 1;
                 OnCheckpoint(1);
-
-                UI.Notify("Reached Checkpoint 1/NAN");
+                
+                UI.ShowSubtitle("Reached Checkpoint 1/NAN");
             }
             if (playerRacer.Position.DistanceTo(Point02) < 10f && CurrentCheckpoint == 1)
             {
@@ -142,38 +178,41 @@ namespace SPraceMod.Races
                 CurrentCheckpoint += 1;
                 OnCheckpoint(3);
 
-                UI.Notify("Reached Checkpoint 3/NAN");
+                UI.ShowSubtitle("Reached Checkpoint 3/NAN");
             }
         }
 
         void OnCheckpoint(int cpointID)
         {
-            for(int i = 0; i < Getcheckpoints().Length(); i++)
-            //_chks = World.CreateBlip(i);
-            Function.Call(Hash.ADD_BLIP_FOR_COORD, i);
+            _chks = World.CreateBlip(Point01);
             Function.Call(Hash.SET_BLIP_COLOUR, _chks.Handle, 1);
             Function.Call(Hash.SET_BLIP_ROUTE, _chks.Handle, true);
 
-             racercarhealthmax = RaceCar01.MaxHealth;
-             racerhealthmax = Racer01.MaxHealth;
-             racerhealth = Racer01.Health;
+            //ShowMessage("Racer health: ~b~" + racerhealthmax + "~w~", 100, Color.Crimson, 2.5f);
+            // _bigmessage = new UIText("Racer health: ~b~" + racerhealthmax, new Point(75, 325), 0.7f, Color.Crimson, 1, true);
+            // _headsupRectangle = new UIRectangle(new Point(0, 320), new Size(200, 110), Color.FromArgb(100, 0, 0, 0));
 
             if (cpointID == 0)
-            {
+            {           
                 if (racercarhealthmax <= 0)
                     racerhealth = 0;
-               // Checkpoint.Position = Point01;     
+             //  Checkpoint.Position = Point01;     
             }
             if (cpointID == 1 && CurrentCheckpoint == 1)
             {
-               // Checkpoint.Position = Point02;
+                _chks.Remove();
+                _chks = World.CreateBlip(Point02);
+                Function.Call(Hash.SET_BLIP_COLOUR, _chks.Handle, 1);
+                Function.Call(Hash.SET_BLIP_ROUTE, _chks.Handle, true);
+
+                // Checkpoint.Position = Point02;
             }
             if (cpointID == 2 && CurrentCheckpoint == 2)
             {
                 //Checkpoint.Remove();
             }
         }
-
+        
         private Vector3 Getcheckpoints()
         {
             var lastDist = float.MaxValue;
@@ -216,6 +255,21 @@ namespace SPraceMod.Races
             Function.Call<bool>(Hash.STAT_GET_INT, name, outArg, -1);
             var cash = outArg.GetResult<int>() + amount;
             Function.Call(Hash.STAT_SET_INT, name, cash, true);
+        }
+
+        public static void ShowMessage(string text, int time, Color color, float size = 2.5f)
+        {          
+            _bigmessage.Caption = text;
+            _bigmessage.Color = color;
+            _bigmessage.Scale = size;
+        }
+
+        private void LogToFile(string text)
+        {
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.Write(text + "\n");
+            }
         }
     }
 }
